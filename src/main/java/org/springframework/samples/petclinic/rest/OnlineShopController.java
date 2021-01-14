45468de5ad5562085eb02d80038d099c1c91e3f5
Avi <Avi@DESKTOP-DSSRJ4F>
2021-01-14 15:21:04 +0530

    api changes

--- src/main/java/org/springframework/samples/petclinic/rest/OnlineShopController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/OnlineShopController.java
@@ -2,20 +2,14 @@ package org.springframework.samples.petclinic.rest;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.model.Product;
-import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.service.OnlineShopServiceImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,7 +23,7 @@ private OnlineShopServiceImpl onlineShopServiceImpl;
 	@PreAuthorize( "hasRole(@roles.ADMIN)" )
 	@GetMapping
 	public ResponseEntity<List<Product>> getProducts(){
-		return null;
+		return onlineShopServiceImpl.getProducts();
 
 	}
 
--- src/main/java/org/springframework/samples/petclinic/service/OnlineShopServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/OnlineShopServiceImpl.java
@@ -1,8 +1,21 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.repository.jpa.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OnlineShopServiceImpl {
 
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public ResponseEntity<List<Product>> getProducts(){
+		return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
+	}
 }
